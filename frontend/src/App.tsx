e26782da1c24f08c8bb85d393db93298e5e9ffdd
Tejas59 <57625719+Tejas59@users.noreply.github.com>
2024-02-17 22:28:25 +0530

    Created Resister form f

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
@@ -20,6 +21,11 @@ const App = () => {
             search page
           </p>
         </Layout>}/>
+        <Route path = "/register" element={
+          <Layout>
+            <Register/>
+          </Layout>
+        } />
         <Route path="*" element={< Navigate to="/"/>}/>
       </Routes>
     </Router>
